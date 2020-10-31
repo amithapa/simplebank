f57eced84b01744e1a313e7256450efa3fd5a238
amithapa <amithapa@devscript>
2020-10-31 12:47:19 +0530

    Test cases.

--- db/sqlc/transfer.sql.go
+++ db/sqlc/transfer.sql.go
@@ -54,7 +54,7 @@ func (q *Queries) GetTransfer(ctx context.Context, id int64) (Transfer, error) {
 	return i, err
 }
 
-const listTansfers = `-- name: ListTansfers :many
+const listTransfers = `-- name: ListTransfers :many
 SELECT id, from_account_id, to_account_id, amount, created_at FROM transfers
 WHERE
     from_account_id = $1 OR
@@ -64,15 +64,15 @@ LIMIT $3
 OFFSET $4
 `
 
-type ListTansfersParams struct {
+type ListTransfersParams struct {
 	FromAccountID int64 `json:"from_account_id"`
 	ToAccountID   int64 `json:"to_account_id"`
 	Limit         int32 `json:"limit"`
 	Offset        int32 `json:"offset"`
 }
 
-func (q *Queries) ListTansfers(ctx context.Context, arg ListTansfersParams) ([]Transfer, error) {
-	rows, err := q.db.QueryContext(ctx, listTansfers,
+func (q *Queries) ListTransfers(ctx context.Context, arg ListTransfersParams) ([]Transfer, error) {
+	rows, err := q.db.QueryContext(ctx, listTransfers,
 		arg.FromAccountID,
 		arg.ToAccountID,
 		arg.Limit,
